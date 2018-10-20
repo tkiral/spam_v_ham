60de534e8ae697eef7d562ec4bfe063edba5ab6e
Taner Kiral <taner.kiral@hotmail.com>
2018-10-19 22:05:41 -0700

    got pull from csv mostly working

--- spam_v_ham.c
+++ spam_v_ham.c
@@ -7,7 +7,7 @@ const char* getfield(char* line, int num)
     const char* tok;
     for (tok = strtok(line, ",");
             tok && *tok;
-            tok = strtok(NULL, ";\n"))
+            tok = strtok(NULL, ",\n"))
     {
         if (!--num)
             return tok;
@@ -19,13 +19,18 @@ int main()
 {
   FILE* stream = fopen("spam.csv", "r");
 
-    char line[][1024];
+//   while(fgets(line,1,stream))
+    int i = 0;
+    char line[1024];
     while (fgets(line, 1024, stream))
     {
         char* tmp = strdup(line);
-        printf("Field 3 would be %s\n", getfield(tmp, 2));
+        // printf("Field 3 would be %s\n", getfield(tmp, 2));
+        strcpy(&line[i],getfield(tmp, 2));
+        printf("inserted this: %s", &line[i++]);
         // NOTE strtok clobbers tmp
         free(tmp);
+        break;
     }
   return 0;
 }
